9bcb6d8cfe1f1d834bfa05ee5efd20eb63261f4a
Utkarsh Tanwar <utkarshtanwar007@gmail.com>
2019-12-11 03:18:19 +0530

    project done!

--- products/urls.py
+++ products/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('',views.products_view,name="products"),
+    path('create/',views.create_view,name="create"),
+    path('<int:product_id>/',views.detail_view,name="detail"),
+    path('<int:product_id>/upvote/',views.upvote_view,name="upvote"),
 ]
